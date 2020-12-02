7cf932f9332e52aa734b9cd01fafe35ba54080b0
Colin Fuller <cjfuller@gmail.com>
2020-12-01 21:23:55 -0500

    Day 1

---
+++ src/d1.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::vec::Vec;
+
+fn read_numbers() -> Vec<i64> {
+    io::BufReader::new(File::open("./data/d1p1.txt").unwrap())
+        .lines()
+        .map(|l| l.unwrap().parse::<i64>().unwrap())
+        .collect()
+}
+
+fn brute_force_sum_two(numbers: &[i64], to: i64) -> Option<(i64, i64)> {
+    for num0 in numbers {
+        for num1 in numbers {
+            if num0 + num1 == to {
+                return Some((*num0, *num1));
+            }
+        }
+    }
+    None
+}
+
+fn brute_force_sum_three(numbers: &[i64], to: i64) -> Option<(i64, i64, i64)> {
+    for num0 in numbers {
+        for num1 in numbers {
+            for num2 in numbers {
+                if num0 + num1 + num2 == to {
+                    return Some((*num0, *num1, *num2));
+                }
+            }
+        }
+    }
+    None
+}
+
+#[allow(dead_code)]
+pub fn solvep1() {
+    let nums = read_numbers();
+    let (n0, n1) = brute_force_sum_two(&nums, 2020).unwrap();
+    println!("{}", n0 * n1)
+}
+
+#[allow(dead_code)]
+pub fn solvep2() {
+    let nums = read_numbers();
+    let (n0, n1, n2) = brute_force_sum_three(&nums, 2020).unwrap();
+    println!("{}", n0 * n1 * n2)
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod d1;
+
+fn main() {
+    //d1::solvep1()
+    d1::solvep2()
+}
