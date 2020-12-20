c3812868da97838653048e63b4d9cb076af58a3b
Colin Fuller <cjfuller@gmail.com>
2020-12-20 14:00:49 -0500

    Day 13, part 2

--- src/d13.main.kts
+++ src/d13.main.kts
@@ -1,29 +1,35 @@
 import java.io.File
 
-fun parseInput(): Pair<Int, List<Pair<Int, Int>>> {
+data class Bus(val number: Long, val offset: Long) {
+    fun arrivesAtCorrectOffset(t: Long): Boolean =
+        (t + offset) % number == 0L
+}
+
+fun parseInput(): Pair<Long, List<Bus>> {
     val lines = File("./data/d13.txt").readLines()
-    val departureTime = Integer.parseInt(lines[0])
+    val departureTime = lines[0].toLong()
     val busses = lines[1]
         .split(",")
         .withIndex()
         .map { (i, v) ->
-            Pair(
-                if (v == "x") {
-                    -1
+            Bus(
+                number = if (v == "x") {
+                    -1L
                 } else {
-                    Integer.parseInt(v)
-                }, i
+                    v.toLong()
+                },
+                offset = i.toLong()
             )
         }
-        .filter { (v, _) -> v != -1 }
+        .filter { (v, _) -> v != -1L }
     return Pair(departureTime, busses)
 }
 
-fun delayForBus(departureTime: Int, bus: Int): Int =
+fun delayForBus(departureTime: Long, bus: Long): Long =
     bus - (departureTime % bus)
 
 val (departureTime, bussesWithOffset) = parseInput()
-val busses = bussesWithOffset.map { it.first }
+val busses = bussesWithOffset.map { it.number }
 val bestBus = busses.minByOrNull { delayForBus(departureTime, it) }!!
 
 println("Part 1:")
@@ -31,4 +37,21 @@ println("Best bus: $bestBus")
 println("Result: ${bestBus * delayForBus(departureTime, bestBus)}")
 
 println("Part 2:")
-println(bussesWithOffset)
+
+val orderedBusses = bussesWithOffset.sortedBy { it.number }.reversed()
+
+fun findTForPair(lhs: Bus, rhs: Bus): Long {
+    var t: Long = (lhs.number - lhs.offset)
+    while (!rhs.arrivesAtCorrectOffset(t)) {
+        t += lhs.number
+    }
+    return t
+}
+
+val result = orderedBusses.reduce { b0, b1 ->
+    val t = findTForPair(b0, b1)
+    val combinedNum = b0.number * b1.number
+    println(t)
+    Bus(number = combinedNum, offset = combinedNum - (t % combinedNum))
+}
+println(result)
